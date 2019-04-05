e663e8c8b3950ea208c511f8f0c41eff156d4e2a
Jacqueline930 <jacqueline.lopez.930@gmail.com>
2019-04-05 15:01:36 -0600

    Created project and implemented api working on it

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+class App extends React.Component {
+ constructor(props){
+   super(props);
+       this.getBook = this.getBook.bind(this);
+ }
+
+ async getBook() {
+  let response = await fetch(`http://xkcd.com/info.0.json`,
+     {'mode': 'no-cors'})
+  let data = await response.json()
+  console.log(data);
+  return data;
 }
+
+ render() {
+   this.getBook()
+   return (
+     <div className="App">
+     <h3>heyy </h3>
+     </div>
+   );
+ }
+}
+
 
 export default App;
